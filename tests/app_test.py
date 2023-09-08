9b9387350e2d13b1bfd5d995afa4f04f29694899
Spiros Boutsis <spirosboutsis@hotmail.com>
2023-09-08 20:56:33 +0300

    update

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,1 +1,12 @@
 from app import app 
+
+
+def test_get_api():
+    with app.test_client() as c:
+        response = c.get('/moto')
+        json_response = response.get_json()
+        assert response.status_code == 200
+
+
+
+        
